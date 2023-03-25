ed6cfb3150e371725e85ddb60a3b18b6a4ebd52d
Trịnh Trưởng <dantakesiav351@gmail.com>
2023-03-25 14:43:13 +0700

    First merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ltddnc_nhom04_k19/SignUp.dart';
+import 'package:ltddnc_nhom04_k19/seller/seller.dart';
 import 'package:ltddnc_nhom04_k19/user/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart';
+import 'dart:convert';
+
 void main() {
   runApp(const MyApp());
 }
@@ -47,12 +52,9 @@ class User {
 
   Map<String, dynamic> toJson() {
     return {
-      "accessToken": this.accessToken,
-      "refreshToken": this.refreshToken,
-      "currentAccount": {
-        "id": this.accountId,
-        "name": this.name
-      }
+      "accessToken": accessToken,
+      "refreshToken": refreshToken,
+      "currentAccount": {"id": accountId, "name": name}
     };
   }
 }
@@ -72,8 +74,10 @@ class LoginPage extends StatelessWidget {
 
   Future<User> login(String email, String password) async {
     final dio = Dio();
-    final response = await dio.post("https://localhost:8000/auth/login",
-        data: '{ "email": "$email", "password": "$password" }',);
+    final response = await dio.post(
+      "https://localhost:8000/auth/login",
+      data: '{ "email": "$email", "password": "$password" }',
+    );
     return User.fromJson(response.data);
   }
 
@@ -82,30 +86,12 @@ class LoginPage extends StatelessWidget {
     String email = "";
     String password = "";
     return Scaffold(
-      resizeToAvoidBottomInset: false,   //new line
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      resizeToAvoidBottomInset: false, //new line
       body: Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
-            new Image.asset(
+            Image.asset(
               'assets/images/laptopbrand.png',
               width: 450.0,
               height: 200.0,
@@ -118,71 +104,89 @@ class LoginPage extends StatelessWidget {
               textScaleFactor: 2.5,
             ),
             const Text(''),
-            SizedBox( // <-- SEE HERE
-                width: 300, child: TextField(
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+            SizedBox(
+                // <-- SEE HERE
+                width: 300,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter username",
+                    icon: Icon(
+                      Icons.account_box,
+                      color: Colors.lightGreenAccent,
+                    ),
+                    labelText: 'Username',
+                    floatingLabelAlignment: FloatingLabelAlignment.start,
+                    floatingLabelStyle: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 16,
+                    ),
+                  ),
+                  onChanged: (value) {
+                    email = value;
+                  },
 
-              ),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Enter username",
-                icon: Icon(Icons.account_box,color: Colors.lightGreenAccent,),
-                labelText: 'Username',
-                floatingLabelAlignment: FloatingLabelAlignment.start,
-                floatingLabelStyle: TextStyle(
-                  color: Colors.amber,
-                  fontSize: 16,
-                ),
-
-              ),
-            )),
+                )),
             const Text(''),
-            SizedBox( // <-- SEE HERE
-                width: 300, child: TextField(
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-
-              ),
-              obscureText: true, // ẩn pass word
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Enter password",
-                icon: Icon(Icons.lock,color: Colors.lightGreenAccent,),
-                labelText: 'Password',
-                floatingLabelAlignment: FloatingLabelAlignment.start,
-                floatingLabelStyle: TextStyle(
-                  color: Colors.amber,
-                  fontSize: 16,
-                ),
-
-              ),
-            )),
+            SizedBox(
+                // <-- SEE HERE
+                width: 300,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  obscureText: true, // ẩn pass word
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter password",
+                    icon: Icon(
+                      Icons.lock,
+                      color: Colors.lightGreenAccent,
+                    ),
+                    labelText: 'Password',
+                    floatingLabelAlignment: FloatingLabelAlignment.start,
+                    floatingLabelStyle: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 16,
+                    ),
+                  ),
+                  onChanged: (value) {
+                    password = value;
+                  },
+                )),
             const Text(''),
             ButtonBar(
               alignment: MainAxisAlignment.center,
-              buttonPadding:EdgeInsets.symmetric(
-                  horizontal: 30,
-                  vertical: 10
-              ),
+              buttonPadding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               children: [
-                ElevatedButton(  child: Row (
-                  children: [
-                    Icon(Icons.login),
-                    Text("Login"),
-
-                  ],
-                ) ,
-                  onPressed: () {
+                ElevatedButton(
+                  child: Row(
+                    children: const [
+                      Icon(Icons.login),
+                      Text("Login"),
+                    ],
+                  ),
+                  onPressed: () async {
+                    User user = await login(email, password);
+                    await setCurrentLogin(user);
+                    User savedUser = await getCurrentLogin();
+                    //TODO: Route to sutable main page
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const UserApp()),
+                      MaterialPageRoute(builder: (context) => const SellerMain()),
                     );
-                  },)],
+                  },
+
+                )
+              ],
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const Text('Does not have account?'),
                 TextButton(
@@ -198,7 +202,6 @@ class LoginPage extends StatelessWidget {
                   },
                 )
               ],
-              mainAxisAlignment: MainAxisAlignment.center,
             )
             // Text(
             //   textDemo,
@@ -206,7 +209,6 @@ class LoginPage extends StatelessWidget {
             // )
           ],
         ),
-
       ),
     );
   }
